17e865e9b164b778cf82f3364bfceeb2d67e9fb7
Bushra <bshra.m@sybertechnology.com>
2024-04-20 12:35:30 +0200

    Added header component and main pages

---
+++ src/Components/Header.js
@@ -0,0 +1,29 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Homepage from "../Pages/Homepage";
+import Menu from "../Pages/Menu";
+import ReserveATable from "../Pages/ReserveATable";
+import AboutUs from "../Pages/AboutUs";
+import Login from "../Pages/Login";
+
+function Header() {
+    return (
+        <div>
+            <nav>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/menu"}>Menu</Link>
+                <Link to={"/reserve"}>Reserve a Table</Link>
+                <Link to={"/about"}>About Us</Link>
+                <Link to={"/login"}>Login</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Homepage/>}></Route>
+                <Route path="/menu" element={<Menu/>}></Route>
+                <Route path="/reserve" element={<ReserveATable/>}></Route>
+                <Route path="/about" element={<AboutUs/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+            </Routes>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Pages/ReserveATable.js
@@ -0,0 +1,5 @@
+function ReserveATable() {
+    return <h1>Reserve A Table</h1>
+}
+
+export default ReserveATable;
