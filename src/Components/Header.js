67c9eec5eb17bea67305b49cdfac03d69da8e3f0
Bushra <bshra.m@sybertechnology.com>
2024-04-30 17:39:36 +0200

    Added styling for header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,4 @@
+import "../Styles/Header.css"
 import { Link, Route, Routes } from "react-router-dom";
 import Homepage from "../Pages/Homepage";
 import Menu from "../Pages/Menu";
@@ -8,8 +9,8 @@ import Login from "../Pages/Login";
 function Header() {
     return (
         <div>
-            <nav>
-                <Link to={"/"}>Home</Link>
+            <nav className="header-nav">
+                <Link to={"/"} style={{color: "white", textDecoration: "none", paddingInlineStart: "10px", paddingInlineEnd: "10px"}}>Home</Link>
                 <Link to={"/menu"}>Menu</Link>
                 <Link to={"/reserve"}>Reserve a Table</Link>
                 <Link to={"/about"}>About Us</Link>
