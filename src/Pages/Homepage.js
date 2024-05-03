312e31bfffd3f6eb63b9df29d6537da3cbb2b71f
Bushra <bshra.m@sybertechnology.com>
2024-05-03 20:10:28 +0200

    Added info section

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,4 +1,8 @@
 import BannerImage from "../assets/home_background_img.jpg";
+import OrganicVegImg from "../assets/organic-vegetables.webp";
+import CoffeeImg from "../assets/coffee.jpg";
+import DeliveryImg from "../assets/delivery.jpg"
+
 
 function Homepage() {
     return (
@@ -12,6 +16,23 @@ function Homepage() {
         </div>
         <div style={{backgroundColor: "#274600", height: "60vh"}}>
             <p style={{fontFamily: "sans-serif", fontSize: "25px", color: "white", textAlign: "center", paddingLeft: "220px", paddingRight: "220px", paddingTop: "25vh"}}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        </div>
+        <div style={{display: "flex", justifyContent: "center", paddingTop: "60px", paddingBottom: "60px"}}>
+            <div style={{width: "20%", textAlign: "center"}}>
+                <img src={OrganicVegImg} style={{height: "200px", width: "200px", objectFit: "cover", borderRadius: "100px", margin: "auto"}}/>
+                <h3>We Use Organic Vegetables</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div style={{width: "20%", textAlign: "center"}}>
+                <img src={CoffeeImg} style={{height: "200px", width: "200px", objectFit: "cover", borderRadius: "100px"}}/>
+                <h3>We Roast Our Own Coffee</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div style={{width: "20%", textAlign: "center"}}>
+            <   img src={DeliveryImg} style={{height: "200px", width: "200px", objectFit: "cover", borderRadius: "100px"}}/>
+                <h3>We Deliver Your Favourit Dishes</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
         </div>
         </>
     )
